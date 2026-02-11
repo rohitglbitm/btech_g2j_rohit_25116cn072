a5d84ac16027693615dbfa48ae1f82b30c5b2178
rohitglbitm <25116cn072@glbitm.ac.in>
2026-02-11 11:23:46 +0530

    Add files via upload

---
+++ greatest among 3 numbers.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+        printf("Enter first integers: \n");
+        scanf("%d", &a);
+        printf("Enter second integers: \n");
+        scanf("%d", &b);
+        printf("Enter third integers: \n");
+        scanf("%d", &c);
+   
+    if(a>b && a>c)
+    {
+         printf("Greatest among all these integer is: %d\n", a);
+    }
+    else if(b>a && b>c)
+    {
+         printf("Greatest among all these integer is: %d\n", b);
+    }
+    else
+    {
+         printf("Greatest among all these integer is: %d\n", c);
+    }
+    
+
+    
+    return 0;
+}
