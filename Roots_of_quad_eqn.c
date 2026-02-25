56479247e85c972fd99039e27b1aea203c275e1d
rohitglbitm <25116cn072@glbitm.ac.in>
2026-02-25 11:28:24 +0530

    Update Roots_of_quad_eqn.c

--- Roots_of_quad_eqn.c
+++ Roots_of_quad_eqn.c
@@ -3,19 +3,19 @@
 
 int main(){
   float a,b,c,r1,r2,D;
-  printf("Standard quadratic equation= Ax^2 + Bx + C = 0\n");
-  printf("Enter coefficient of 'x^2='\n",a);
+  printf("Standard quadratic equation: Ax^2 + Bx + C = 0\n");
+  printf("Enter the value of A: ",a);
   scanf("%f", &a);
-  printf("Enter coefficient of 'x='\n",b);
+  printf("Enter the value of B: ",b);
   scanf("%f", &b);  
-  printf("Enter the value of c ='\n",c);
+  printf("Enter the value of C: ",c);
   scanf("%f", &c);
     D = b*b - 4*a*c;
 if(D>0){
     r1 = (-b + sqrt(D))/(2*a);
     r2 = (-b - sqrt(D))/(2*a);
-    printf("root 1 =%.2f,", r1);
-    printf("root 2 = %.2f", r2);
+    printf("First root is %.2f\n", r1);
+    printf("Second root is %.2f", r2);
 }
 else if(D == 0){
     r2 = (-b/(2*a));
