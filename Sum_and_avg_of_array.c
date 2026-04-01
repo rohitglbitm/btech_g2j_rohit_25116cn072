f6bd48c3f338e4e80f2b4986e79ad73ebd00a5c2
rohitglbitm <25116cn072@glbitm.ac.in>
2026-04-01 11:30:45 +0530

    Add files via upload

---
+++ Sum_and_avg_of_array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    float sum = 0.0, average;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d numbers:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for (i = 0; i < n; i++) {
+        sum += arr[i]; 
+    }
+
+    average = sum / n; 
+    printf("\nSum = %.2f\n", sum);
+    printf("Average = %.2f\n", average);
+
+    return 0;
+}
+
+
